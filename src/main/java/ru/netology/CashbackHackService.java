d8b042079dce2ee93f52f9ea21d0fa3f3c9f160c
EvgeniaZlobina <ZEA_6590@mail.ru>
2023-07-20 20:57:20 +0500

    repeat

--- src/main/java/ru/netology/CashbackHackService.java
+++ src/main/java/ru/netology/CashbackHackService.java
@@ -2,7 +2,8 @@ package ru.netology;
 
 public class CashbackHackService {
     private final int boundary = 1000;
-// расчет бонусов
+
+
     public int remain(int amount) {
         return boundary - amount % boundary;
     }
